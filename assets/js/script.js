f508570036e4db9e825203f378f79e07df10e6e3
Heron Amaral <heron.amaral@gmail.com>
2021-11-25 20:30:37 -0300

    Visible change of the current instrument

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,9 @@ function playComposition(songArr) {
 }
 
 function changeInstrument(inst) {
+    document.querySelector('.playing').classList.remove('playing');
+    document.querySelector(`h1[folder="${inst}"]`).classList.add('playing');
+
     document.querySelector('.audios').innerHTML = `<audio id="s_keyq" src="sounds/${inst}/keyq.wav"></audio><audio id="s_keyw" src="sounds/${inst}/keyw.wav"></audio><audio id="s_keye" src="sounds/${inst}/keye.wav"></audio><audio id="s_keya" src="sounds/${inst}/keya.wav"></audio><audio id="s_keys" src="sounds/${inst}/keys.wav"></audio><audio id="s_keyd" src="sounds/${inst}/keyd.wav"></audio><audio id="s_keyz" src="sounds/${inst}/keyz.wav"></audio><audio id="s_keyx" src="sounds/${inst}/keyx.wav"></audio><audio id="s_keyc" src="sounds/${inst}/keyc.wav"></audio>`;
     
 }
